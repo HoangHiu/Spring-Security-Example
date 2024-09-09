d4959702a9af2b7d44e45442982689c618af8e6e
Hoang Hieu <hoangdinhhieu2105.work@gmail.com>
2024-09-09 11:19:57 +0700

    Initial commit

---
+++ src/main/java/org/myapp/spirngsecurityexample/SpirngSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package org.myapp.spirngsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngSecurityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngSecurityExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myapp/spirngsecurityexample/SpirngSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myapp.spirngsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngSecurityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
