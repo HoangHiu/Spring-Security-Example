8d4505abbff62272eca7c645e808b9c0ebac2f06
Hoang Hieu <hoangdinhhieu2105.work@gmail.com>
2024-09-17 23:02:26 +0700

    finish basic config

--- src/main/java/org/myapp/spirngsecurityexample/configuration/SecurityConfiguration.java
+++ src/main/java/org/myapp/spirngsecurityexample/configuration/SecurityConfiguration.java
@@ -1,13 +1,53 @@
 package org.myapp.spirngsecurityexample.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@Slf4j
 public class SecurityConfiguration {
-    public SecurityFilterChain securityFilterChain(){
-        
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("GET", "/v2/book").authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("pwd1"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/org/myapp/spirngsecurityexample/controller/BookController.java
+++ src/main/java/org/myapp/spirngsecurityexample/controller/BookController.java
@@ -5,6 +5,7 @@ import org.myapp.spirngsecurityexample.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,11 @@ public class BookController {
 
     @GetMapping(value = "")
     public ResponseEntity<List<BookEntity>> getAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<List<BookEntity>> getAllBooksPost(){
         return ResponseEntity.ok(bookService.getAllBooks());
     }
 }
